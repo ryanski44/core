f09bf94b040698fd0fb658795a65294c83c6fdae
Ryan Adams <ryan@rbadams.com>
2025-07-13 06:23:27 +0000

    fix deprecation bug

--- custom_components/com_rbadams_thermostat/__init__.py
+++ custom_components/com_rbadams_thermostat/__init__.py
@@ -33,12 +33,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+    platforms_to_forward = [
+        platform for platform in PLATFORMS if entry.options.get(platform, True)
+    ]
+    coordinator.platforms.extend(platforms_to_forward)
+    await hass.config_entries.async_forward_entry_setups(entry, platforms_to_forward)
 
     entry.add_update_listener(async_reload_entry)
 
